f301ab90cb8c43325ed2b162f2d04c321842f3a5
Sk7Str1p3 <finenkofedor@gmail.com>
2025-05-25 18:22:51 +0000

    attempt on build fix

--- build.rs
+++ build.rs
@@ -1,7 +1,23 @@
 use meson_next as meson;
-use std::{collections::HashMap, env, path::PathBuf };
+use std::collections::HashMap;
+use std::env;
+use std::path::PathBuf;
 
+// This code calls meson to build project
 fn main() {
+    // Because this code works like this:
+    //
+    // cargo build => build.rs => meson => cargo build => build.rs
+    //
+    // we must prevent build from launching more than one time
+    // or this would lead to infinite compilation
+    let lever = "isBuildStarted";
+    if env::var(lever).unwrap_or(0.to_string()) == "1" {
+        println!("Build is already startred! Exiting");
+        return;
+    }
+    env::set_var(lever, "1");
+
     let build_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("build");
     let build_path = build_path.to_str().unwrap();
 
